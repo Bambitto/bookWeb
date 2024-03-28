11cd19a0557324a4e7c013d30ff67df15c036f98
Pawel Borkowski <pborkowski@openskydata.com>
2024-03-28 21:36:11 +0100

    update db model

--- bookWeb/Data/AppDbContext.cs
+++ bookWeb/Data/AppDbContext.cs
@@ -7,20 +7,50 @@ namespace bookWebApi.Data
     {
 
         public DbSet<Book> Books => Set<Book>();
+        public DbSet<Review> Reviews => Set<Review>();
+        public DbSet<User> Users => Set<User>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Review>()
+                .HasOne(b => b.Book)
+                .WithMany(r => r.Reviews)
+                .HasForeignKey(b => b.BookId);
+            
+            modelBuilder.Entity<Review>()
+                .HasOne(u => u.User)
+                .WithMany(r => r.Reviews)
+                .HasForeignKey(u => u.UserId);
+
+            modelBuilder.Entity<Review>()
+                .Property(x => x.Comment).HasMaxLength(512);
+
+
             modelBuilder.Entity<Book>()
                 .Property(x => x.Title).IsRequired().HasMaxLength(64);
 
             modelBuilder.Entity<Book>()
-                .Property(x=> x.Author).IsRequired().HasMaxLength(64);
+                .Property(x => x.Author).IsRequired().HasMaxLength(64);
 
             modelBuilder.Entity<Book>()
-                .Property(x=> x.Description).HasMaxLength(512);
+                .Property(x => x.Description).HasMaxLength(512);
 
             modelBuilder.Entity<Book>()
                 .Property(x => x.Genre).IsRequired();
+
+
+            modelBuilder.Entity<User>()
+                .Property(x => x.Email).IsRequired().HasMaxLength(64);
+
+            modelBuilder.Entity<User>()
+                .Property(x => x.FirstName).IsRequired().HasMaxLength(64);
+
+            modelBuilder.Entity<User>()
+                .Property(x => x.LastName).HasMaxLength(64);
+
+            modelBuilder.Entity<User>()
+                .Property(x => x.Password).IsRequired();
+
         }
     }
 }
---
+++ bookWeb/Migrations/20240328203336_new_entities.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bookWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class new_entities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Score = table.Column<double>(type: "float", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: true),
+                    BookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_BookId",
+                table: "Reviews",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
