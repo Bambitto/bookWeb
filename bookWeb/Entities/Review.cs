63ac5689cd0a39d6734abe53a60f9c5d8472f3e7
Pawel Borkowski <pborkowski@openskydata.com>
2024-03-28 21:10:06 +0100

    add user and review entity add correlation

---
+++ bookWeb/Entities/Review.cs
@@ -0,0 +1,15 @@
+﻿namespace bookWebApi.Entities
+{
+    public class Review
+    {
+        public Guid Id { get; set; }
+        public required double  Score { get; set; }
+        public string? Comment { get; set; }
+
+        public Guid BookId { get; set; }
+        public Guid UserId { get; set; }
+
+        public Book Book { get; set; }
+        public User User { get; set; }
+    }
+}
--- bookWeb/Entities/User.cs
+++ bookWeb/Entities/User.cs
@@ -2,5 +2,12 @@
 {
     public class User
     {
+        public Guid Id {  get; set; }
+        public required string Email { get; set; }
+        public required string FirstName { get; set; }
+        public string? LastName { get; set; }
+        public required string Password { get; set; }
+
+        public List<Review> Reviews { get; set; }
     }
 }
